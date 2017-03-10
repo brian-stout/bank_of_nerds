763a1f91811fc22b465ab071012fb13d2f539c6e
Brian Stout <brianjonstout@gmail.com>
2017-03-10 10:38:34 -0500

    Start basic account classes.

--- accounts/account.py
+++ accounts/account.py
@@ -1,6 +1,14 @@
 class Account():
 
-    def __init__(self):
-        pass
+    numberOfAccounts = 0
+    accountLookup = dict()
+
+    def __init__(self, name):
+        self._accountName = name
+        self._moneyInAccount = 0
+        self._actID = Account.numberOfAccounts
+        Account.numberOfAccounts += 1
+        Account.accountLookup[self._actID] = self
+        
 
 if __name__ == '__main__': pass
